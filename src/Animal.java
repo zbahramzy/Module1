d31136541021cff5b5a5fdf9ee5f284bb0d02ec0
zalandbahramzy <Studio123>
2021-09-20 15:29:50 +0200

    second update

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,21 @@
 public class Animal {
+    // position of animals
     private int xCoordinate;
     private int yCoordinate;
+
+    public Animal(int xInitialCoordinate, int yInitialCoordinate) {
+        setXCoordinate(xInitialCoordinate);
+        setYCoordinate(yInitialCoordinate);
+    }
+
+    public int getXCoordinate() { return xCoordinate; }
+    public int getYCoordinate() { return yCoordinate; }
+
+    private void setXCoordinate(int xCoordinate) { this.xCoordinate = xCoordinate; }
+    private void setYCoordinate(int yCoordinate) { this.yCoordinate = yCoordinate; }
+
+    public void move(int xAmountOfChange, int yAmountOfChange) {
+
+    }
 
 }
