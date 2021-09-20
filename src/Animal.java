44a2abf7e0382f366400a084439aa09e016513ba
zalandbahramzy <Studio123>
2021-09-20 16:30:16 +0200

    forth update

--- src/Animal.java
+++ src/Animal.java
@@ -3,25 +3,29 @@ public class Animal {
     private int xCoordinate;
     private int yCoordinate;
 
-    // constructor
+    // constructor, initialises animal position
     public Animal(int xInitialCoordinate, int yInitialCoordinate) {
         setXCoordinate(xInitialCoordinate);
         setYCoordinate(yInitialCoordinate);
     }
 
-    // getters,
+    // getters, return current animal position
     public int getXCoordinate() { return xCoordinate; }
     public int getYCoordinate() { return yCoordinate; }
-    // setters,
+    // setters, update animal position
     private void setXCoordinate(int xCoordinate) { this.xCoordinate = xCoordinate; }
     private void setYCoordinate(int yCoordinate) { this.yCoordinate = yCoordinate; }
 
+    // animal speak method
     public void speak(String sentence) {
         System.out.println(sentence);
     }
 
+    // animal move method
     public void move(int xChange, int yChange) {
+        // establish game borders
         if (((this.xCoordinate+xChange)>=1 && (this.xCoordinate+xChange)<=10) && ((this.yCoordinate+yChange)>=1 && (this.yCoordinate+yChange)<=10)) {
+            // establish allowable move, cannot move animal by more than 1 in x and y direction
             if ((xChange <= 1 && xChange >= -1) && (yChange <= 1 && yChange >= -1) && xChange != 0 || yChange != 0) {
                 setXCoordinate(this.xCoordinate + xChange);
                 setYCoordinate(this.yCoordinate + yChange);
