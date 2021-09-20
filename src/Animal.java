27034899b72cb3364ecda1220c2a2f9c745dfaf5
zalandbahramzy <Studio123>
2021-09-20 16:24:11 +0200

    Third update

--- src/Animal.java
+++ src/Animal.java
@@ -3,19 +3,35 @@ public class Animal {
     private int xCoordinate;
     private int yCoordinate;
 
+    // constructor
     public Animal(int xInitialCoordinate, int yInitialCoordinate) {
         setXCoordinate(xInitialCoordinate);
         setYCoordinate(yInitialCoordinate);
     }
 
+    // getters,
     public int getXCoordinate() { return xCoordinate; }
     public int getYCoordinate() { return yCoordinate; }
-
+    // setters,
     private void setXCoordinate(int xCoordinate) { this.xCoordinate = xCoordinate; }
     private void setYCoordinate(int yCoordinate) { this.yCoordinate = yCoordinate; }
 
-    public void move(int xAmountOfChange, int yAmountOfChange) {
-
+    public void speak(String sentence) {
+        System.out.println(sentence);
     }
 
+    public void move(int xChange, int yChange) {
+        if (((this.xCoordinate+xChange)>=1 && (this.xCoordinate+xChange)<=10) && ((this.yCoordinate+yChange)>=1 && (this.yCoordinate+yChange)<=10)) {
+            if ((xChange <= 1 && xChange >= -1) && (yChange <= 1 && yChange >= -1) && xChange != 0 || yChange != 0) {
+                setXCoordinate(this.xCoordinate + xChange);
+                setYCoordinate(this.yCoordinate + yChange);
+            }
+            else {
+                System.out.println("Invalid move!");
+            }
+        }
+        else {
+            System.out.println("Border reached!");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
 public class Main {
+    public static void main(String[] args) {
+        // Create rabbit object and snake object, with initial positions
+        Animal rabbit = new Animal(1,1);
+        Animal snake = new Animal(10,10);
+
+        rabbit.speak("I am the Rabbit, my initial position is: " + rabbit.getXCoordinate() + ", " + rabbit.getYCoordinate());
+        snake.speak("I am the Snake, my initial position is: " + snake.getXCoordinate() + ", " + snake.getYCoordinate() + "\n");
+
+        //the state of the rabbit
+        boolean isRabbitAlive = true;
+        while (isRabbitAlive) {
+            //if the snake and rabbit within 1 square of each other, only the snake is going to move
+            if (Math.abs(snake.getXCoordinate()-rabbit.getXCoordinate()) == 1 && Math.abs(snake.getYCoordinate()-rabbit.getYCoordinate()) == 1) {
+                rabbit.speak("Noooooo Please don't eat me!!");
+                snake.move(-1, -1);
+                snake.speak("I am the Snake, I'm now standing on square " + snake.getXCoordinate() + ", " + snake.getYCoordinate());
+                snake.speak("Ahhrrrrrrr I'm eating you ha ha!");
+                isRabbitAlive = false;
+            }
+            else { //the snake and rabbit is moving and saying their positions
+                rabbit.move(1, 0);
+                rabbit.speak("I am the Rabbit, I'm now standing on square " + rabbit.getXCoordinate() + ", " + rabbit.getYCoordinate());
+                snake.move(0, -1);
+                snake.speak("I am the Snake, I'm now standing on square " + snake.getXCoordinate() + ", " + snake.getYCoordinate());
+            }
+        }
+    }
 }
